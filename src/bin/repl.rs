c86d63cecb3e398d538e34a78387b05f1e6ac25a
Hunter Praska <hunter@wiggin-labs.com>
2017-11-15 19:15:39 -0600

    Allow multiline input in repl

--- src/bin/repl.rs
+++ src/bin/repl.rs
@@ -11,6 +11,11 @@ fn main() {
         // Read input
         let mut input = String::new();
         stdin().read_line(&mut input).unwrap();
+        while input.chars().filter(|&c| c == '(').count()
+            > input.chars().filter(|&c| c == ')').count()
+        {
+            stdin().read_line(&mut input).unwrap();
+        }
         let tokens = scheme::Parser::parse(&input);
         let objects = scheme::Token::build_ast(tokens);
         for object in objects {
